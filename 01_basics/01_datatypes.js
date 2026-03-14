ae7fd5b4d0ccc1c6a5fea417c09e90db95b92f3d
Banti Prasad Sahoo <bantiprasadsahoo2004@gmail.com>
2026-03-14 15:03:00 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer verison
+
+// alert(3 + 3 ) // we are using node.js ,not brower
+
+console.log(3+3); // code readability should be high
+console.log("Banti");
+
+
+let name = "Banti" // ''string value
+let age = 22 // number value
+let isLoggedIn = false
+let state;
+
+//number =>2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null=> standlone value
+//undefined =>
+//symbol=> unique
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
