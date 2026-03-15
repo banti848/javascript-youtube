02a27f4d96373961f1c20945a9a831f501c97752
Banti Prasad Sahoo <bantiprasadsahoo2004@gmail.com>
2026-03-15 06:58:01 +0000

    add

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,62 @@
+// let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof(score)); 
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+// let score = null
+
+// console.log(typeof score);
+// console.log(typeof(score)); 
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// let score = undefined
+
+// console.log(typeof score);
+// console.log(typeof(score)); 
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+// let score = true
+
+// console.log(typeof score);
+// console.log(typeof(score)); 
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// let score = false
+
+// console.log(typeof score);
+// console.log(typeof(score)); 
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let score = "Banti"
+
+console.log(typeof score);
+console.log(typeof(score)); 
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" =>NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = 
