800b6a29a27b0004a8be39630a1616a76bfe3a12
Banti Prasad Sahoo <bantiprasadsahoo2004@gmail.com>
2026-03-15 14:19:35 +0000

    conversion of data types

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -45,18 +45,46 @@
 // console.log(typeof valueInNumber);
 // console.log(valueInNumber);
 
-let score = "Banti"
+// let score = "Banti"
 
-console.log(typeof score);
-console.log(typeof(score)); 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof score);
+// console.log(typeof(score)); 
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" =>NaN
 // true => 1; false => 0
 
-let isLoggedIn = 1
+// let isLoggedIn = 1
 
-let booleanIsLoggedIn = 
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// let isLoggedIn = 0
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// let isLoggedIn = ""
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// let isLoggedIn = "banti"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1=> true 0 => false
+// "" => false 
+// "Banti" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
