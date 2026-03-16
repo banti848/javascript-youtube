5c12660a090cb01901ff45c12315f593693ea4ed
Banti Prasad Sahoo <bantiprasadsahoo2004@gmail.com>
2026-03-16 06:12:29 +0000

    datatype

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -127,6 +127,17 @@ let negValue = -value
 // gameCounter++;
 // console.log(gameCounter);
 
-let gameCounter = 100
-++gameCounter;
-console.log(gameCounter);
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
+
+let x = 3;
+const y = x++; // postfix
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a; // prefix
+
+console.log(`a:${a}, b:${b}`);
---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,35 @@
+// premitive 
+// 7 types: string , num, boolean,null , undefined ,symbol , BigInt , 
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null 
+let userEmail;
+
+// const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+const bigNumber = 2333334567654345678765432345676543234567654n
+// console.log(bigNumber)
+// JavaScript is a dynamically typed language,
+// reference type ( Non premitive)
+
+// Array , Objects , Functions // function output object hota hai 
+const heros = ['shaktiman','naaraj', 'King' ]
+ let myObj = {
+    name: "Banti",
+    age: 22,
+}
+
+const myFunction=function(){
+    console.log("Hello world");
+}
+console.log(typeof bigNumber);
+console.log(typeof outsideTemp);
+console.log(typeof scoreValue);
+console.log(typeof myFunction);
+console.log(typeof heros);
+console.log(typeof anotherId);
