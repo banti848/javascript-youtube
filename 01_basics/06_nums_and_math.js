1ed56db24af3afe8ba433b6978d460e0458523b7
Banti Prasad Sahoo <bantiprasadsahoo2004@gmail.com>
2026-03-17 15:37:58 +0000

    add

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,27 @@
+const score = 4000
+// console.log(score);
+
+const balance = new Number(100.22);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+// const otherNumber = 23.456
+// console.log(otherNumber.toPrecision(3))
+// const otherNumber = 123.456
+// console.log(otherNumber.toPrecision(3))
+// const otherNumber = 1123.456
+// console.log(otherNumber.toPrecision(4))
+
+// const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-In'))
+
+
+// ++++++++++++++++++++++\\*MATH*//++++++++++++++++++//
+
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(43.3))
