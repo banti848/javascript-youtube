d1897bc96c2558468f7cd24d81910d11635d210c
Banti Prasad Sahoo <bantiprasadsahoo2004@gmail.com>
2026-03-17 03:45:50 +0000

    string add

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -1,9 +1,34 @@
-const name = "Banti"
-const repocount = 50 
-// console.log(name + repocount + "Value" );
-console.log(`Hello my name is ${name} and my repo count is ${repocount}`)
+// const name = "Banti"
+// const repocount = 50 
+// // console.log(name + repocount + "Value" );
+// console.log(`Hello my name is ${name} and my repo count is ${repocount}`)
 
-const gameName = new String('Banti')
+const gameName = new String('Banti-Eccc-fcccc')
 
-console.log(gameName[0]);
-console.log(gameName.__proto__);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3)) //position pe kiya charcter janeke liye
+// console.log(gameName.indexOf("a"));//kis postion pe ata hai 
+// console.log(gameName.substring());
+
+// const newString = gameName.substring(0,4) // 0 to 3 charcter not include 4 
+// console.log(newString)
+
+// const anotherString = gameName.slice(-8,4)
+// console.log(anotherString)
+
+// const newStringOne = "   Banti   "
+// console.log(newStringOne)
+// console.log(newStringOne.trim());//trim-starting space and ending space hota deti hai 
+// const url = "https://Banti.com/Banti%20PrasadSahoo"
+// console.log(url.replace('%20','-'));
+
+
+// console.log(url.includes('Banti'))
+
+// console.log (gameName.split('-'))
+
+console.log(gameName.small());
