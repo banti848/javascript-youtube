7451c62a0683737a40eeb310ebd80128e9cb5371
Banti Prasad Sahoo <bantiprasadsahoo2004@gmail.com>
2026-03-19 07:22:35 +0000

    add

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -23,5 +23,21 @@ const balance = new Number(100.22);
 
 
 console.log(Math);
-console.log(Math.abs(-4));
-console.log(Math.round(43.3))
+// console.log(Math.abs(-4)); //absoulte value
+// console.log(Math.round(43.3));
+// console.log(Math.ceil(43.2));
+// console.log(Math.floor(43.9));
+// console.log(Math.min(43,2,4,55,566,78));
+// console.log(Math.max(23,45,2,9,222,2,1,56789,999));
+console.log(Math.random());// 0 to 1 value show heba
+console.log((Math.random()*19 )+ 22);
+console.log(Math.floor(Math.random()*10)+1);
+
+
+const min = 10
+const max = 20 
+  
+console.log(Math.random() * (max - min + 1))
+console.log(Math.floor(Math.random()*(max - min + 1)))
+console.log(Math.floor(Math.random()*(max - min + 1)) + min )
+console.log(Math.floor(Math.random()*(max - min + 1)) + max )
