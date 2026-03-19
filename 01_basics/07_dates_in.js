024847611356d5fca803e6cbcf2b8032e8169b7e
Banti Prasad Sahoo <bantiprasadsahoo2004@gmail.com>
2026-03-19 14:12:05 +0000

    add

--- 01_basics/07_dates_in.js
+++ 01_basics/07_dates_in.js
@@ -1,7 +1,24 @@
 // Dates 
 let myDate = new Date()
-console.log(myDate)
-console.log(myDate.toString());
-console.log(myDate.toDateString());
-console.log(myDate.toLocaleString());
-console.log(typeof myDate);
+// console.log(myDate)
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2026, 2,19);
+// console.log(myCreatedDate.toDateString());
+
+
+// let myCreatedDate = new Date(2026, 2,19,2,4);
+// console.log(myCreatedDate.toLocaleString());
+
+// let myCreatedDate = new Date(2026, 2,19,2,4);
+// console.log(myCreatedDate.toLocaleDateString());
+
+// let myCreatedDate = new Date("2026-03-19");
+// console.log(myCreatedDate.toLocaleDateString());
+
+let myTimestamp = Date.now()
+
+console.log(myTimestamp);
